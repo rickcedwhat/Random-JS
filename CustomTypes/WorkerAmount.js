ab8e6e0a08ec6c8609f0e36618d302fef68a5d07
rickcedwhat <46553325+rickcedwhat@users.noreply.github.com>
2023-02-01 17:43:58 -0500

    Update WorkerAmount.js

--- CustomTypes/WorkerAmount.js
+++ CustomTypes/WorkerAmount.js
@@ -1,60 +1,72 @@
 class WorkerAmount {
-    /**
-     * @type {string}
-     */
-    workerAmountID;
-    /**
-     * @type {number | null}
-     */
-    amount;
-    /**
-     * @type {number | null}
-     */
-    estimate;
-    /**
-     * @type {string}
-     */
-    paymentStatusDisplay;
-    /**
-     * @type {string}
-     */
-    requestStatusDisplay;
-    /**
-     * @type {string}
-     */
-    requestedByDisplay;
-    /**
-     * @type {string}
-     */
-    workerID;
-    /**
-     * @type {string}
-     */
-    workerUserID;
-    /**
-     * @type {string}
-     */
-    workerFullName;
-    /**
-     * @type {string}
-     */
-    workerAvatarColor;
-    /**
-     * @type {string}
-     */
-    workerEmail;
-
-    constructor({workerAmountID, amount, estimate, paymentStatusDisplay, requestStatusDisplay, requestedByDisplay, workerID, workerUserID, workerFullName, workerAvatarColor, workerEmail}) {
-        this.workerAmountID = workerAmountID;
-        this.amount = amount;
-        this.estimate = estimate;
-        this.paymentStatusDisplay = paymentStatusDisplay;
-        this.requestStatusDisplay = requestStatusDisplay;
-        this.requestedByDisplay = requestedByDisplay;
-        this.workerID = workerID;
-        this.workerUserID = workerUserID;
-        this.workerFullName = workerFullName;
-        this.workerAvatarColor = workerAvatarColor;
-        this.workerEmail = workerEmail;
+    constructor(bubbleObject) {
+        if (customObjectsCache.has(bubbleObject)) {
+            return customObjectsCache.get(bubbleObject);
+        }
+        this._workerAmount=bubbleObject;
+        this._workerAmountID;
+        this._amount;
+        this._estimate;
+        this._paymentStatusDisplay;
+        this._requestStatusDisplay;
+        this._worker;
+        customObjectsCache.set(bubbleObject, this);
     }
+
+    get allProperties(){
+        if (isEmpty(this._allProperties)) {
+            this._allProperties = this._workerAmount.listProperties();
+        }
+        return this._allProperties;
+    }
+    
+    get id() {
+        if (isEmpty(this._workerAmountID)) {
+            this._workerAmountID = this._workerAmount.get("_id");
+        }
+        return this._workerAmountID;
+    }
+
+    //not tested
+    get amount() {
+        if (isEmpty(this._amount)) {
+            this._amount = this._workerAmount.get("amount_number");
+        }
+        return this._amount;
+    }
+
+    //not tested
+    get estimate() {
+        if (isEmpty(this._estimate)) {
+            this._estimate = this._workerAmount.get("estimate_number");
+        }
+        return this._estimate;
+    }
+
+    //not tested
+    get paymentStatusDisplay() {
+        if (isEmpty(this._paymentStatusDisplay)) {
+            let status = this._workerAmount.get("payment_status_option");
+            this._paymentStatusDisplay = status.get("display");
+        }
+        return this._paymentStatusDisplay;
+    }
+
+    //not tested
+    get requestStatusDisplay() {
+        if (isEmpty(this._requestStatusDisplay)) {
+            let status = this._workerAmount.get("request_status_option");
+            this._requestStatusDisplay = status.get("display");
+        }
+        return this._requestStatusDisplay;
+    }
+
+    //not tested
+    get worker() {
+        if (isEmpty(this._worker)) {
+            this._worker = this._workerAmount.get("worker_custom_worker");
+        }
+        return this._worker;
+    }
+
 }
